171cd735b934373708f686577fb6a2f7995a7ece
dennismozart1994 <dennis.mozart@gmail.com>
2020-07-19 17:33:20 -0300

    More Skill Component
    Just a few Widget Updates based on the tab that is clicked

--- Source/Study/Private/ActionsHUD.cpp
+++ Source/Study/Private/ActionsHUD.cpp
@@ -2,4 +2,73 @@
 
 
 #include "ActionsHUD.h"
+#include "Components/Button.h"
+
+bool UActionsHUD::Initialize()
+{
+    bool success = Super::Initialize();
+    if(!success) return false;
+
+    if(!ensure(WarriorButton != nullptr) && !ensure(ArcherButton != nullptr) && !ensure(MageButton != nullptr)) return false;
+    WarriorButton->OnClicked.AddDynamic(this, &UActionsHUD::OnWarriorClicked);
+    ArcherButton->OnClicked.AddDynamic(this, &UActionsHUD::OnArcherClicked);
+    MageButton->OnClicked.AddDynamic(this, &UActionsHUD::OnMageClicked);
+    return success;
+}
+
+void UActionsHUD::NativeConstruct()
+{
+    Super::NativeConstruct();
+
+    if(CurrentTab == ESkillClass::SC_Warrior) SetTabAsActive(WarriorButton);
+    if(CurrentTab == ESkillClass::SC_Archier) SetTabAsActive(ArcherButton);
+    if(CurrentTab == ESkillClass::SC_Magician) SetTabAsActive(MageButton);
+    
+    WarriorButton->SetVisibility(ESlateVisibility::Hidden);
+    ArcherButton->SetVisibility(ESlateVisibility::Hidden);
+    MageButton->SetVisibility(ESlateVisibility::Hidden);
+
+    for(ESkillClass Tab: TabsAvailable)
+    {
+        if(Tab == ESkillClass::SC_Warrior) WarriorButton->SetVisibility(ESlateVisibility::Visible);
+
+        if(Tab == ESkillClass::SC_Archier) ArcherButton->SetVisibility(ESlateVisibility::Visible);
+
+        if(Tab == ESkillClass::SC_Magician) MageButton->SetVisibility(ESlateVisibility::Visible);
+    }
+}
+
+void UActionsHUD::OnWarriorClicked()
+{
+    SetTabAsActive(WarriorButton);
+    SetTabAsDeactivated(ArcherButton);
+    SetTabAsDeactivated(MageButton);
+}
+
+void UActionsHUD::OnArcherClicked()
+{
+    SetTabAsDeactivated(WarriorButton);
+    SetTabAsActive(ArcherButton);
+    SetTabAsDeactivated(MageButton);
+}
+
+void UActionsHUD::OnMageClicked()
+{
+    SetTabAsDeactivated(WarriorButton);
+    SetTabAsDeactivated(ArcherButton);
+    SetTabAsActive(MageButton);
+}
+
+void UActionsHUD::SetTabAsActive(UButton* Button)
+{
+    Button->WidgetStyle.Normal.TintColor = ActiveColour;
+}
+
+void UActionsHUD::SetTabAsDeactivated(UButton* Button)
+{
+    Button->WidgetStyle.Normal.TintColor = NormalColour;
+}
+
+
+
 
--- Source/Study/Public/ActionsHUD.h
+++ Source/Study/Public/ActionsHUD.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "Blueprint/UserWidget.h"
+#include "CustomVariables.h"
 #include "ActionsHUD.generated.h"
 
 /**
@@ -13,5 +14,37 @@ UCLASS()
 class STUDY_API UActionsHUD : public UUserWidget
 {
 	GENERATED_BODY()
+
+	public:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Skills|Tab")
+    TArray<ESkillClass> TabsAvailable;
 	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Skills|Tab")
+	ESkillClass CurrentTab;
+
+	UPROPERTY(BlueprintReadWrite, meta = (BindWidget))
+	class UButton* WarriorButton;
+
+	UPROPERTY(BlueprintReadWrite, meta = (BindWidget))
+    class UButton* ArcherButton;
+
+	UPROPERTY(BlueprintReadWrite, meta = (BindWidget))
+    class UButton* MageButton;
+
+	UFUNCTION()
+	void OnWarriorClicked();
+
+	UFUNCTION()
+    void OnArcherClicked();
+
+	UFUNCTION()
+    void OnMageClicked();
+
+	protected:
+    virtual bool Initialize() override;
+	virtual void NativeConstruct() override;
+	const FSlateColor ActiveColour = FSlateColor(FLinearColor(0.08f, 0.08f, 0.08f, 1.f));
+	const FSlateColor NormalColour = FSlateColor(FLinearColor(1.f, 1.f, 1.f, 1.f));
+	void SetTabAsActive(class UButton* Button);
+	void SetTabAsDeactivated(class UButton* Button);
 };
